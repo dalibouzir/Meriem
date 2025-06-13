f4f7b38d2597e01757585c439d69d3eea8131e56
mohamedali bouzir <bouzirdali@gmail.com>
2025-06-13 22:49:49 +0100

    Responsive

---
+++ src/app/head.tsx
@@ -0,0 +1,12 @@
+// app/head.tsx
+export default function Head() {
+    return (
+      <>
+        <title>EmotionAI</title>
+        <meta name="description" content="Empowering Scalable Emotional Support" />
+        <meta name="viewport" content="width=device-width, initial-scale=1" />
+        <link rel="icon" href="/icon.png" />
+      </>
+    )
+  }
+  
